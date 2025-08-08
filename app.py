3485520b9a8126e1a64fd8b28b027e0448978184
KathaCamacho <katha.camacho@gmail.com>
2025-08-07 22:57:10 -0500

    nueva funcion para cargar datos

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pandas as pd
 import datetime as dt
 
 
-
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
@@ -21,8 +20,10 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
-    
+    df = pd.read_csv("datos_energia.csv")
+    df['time'] = pd.to_datetime(df['time'])
+    df.set_index('time', inplace=True)
+    return df    
 
 # Cargar datos
 data = load_data()
@@ -240,4 +241,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
